be3947e82d582780fd35f5e3c3945a941cd110d0
Rinvernal <irina.elkina2006@gmail.com>
2024-11-03 20:53:22 +0200

    ready to start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,8 @@
-import Profile from "./Profile/Profile";
-import userData from "../userData.json";
-import friends from "../friends.json"
-import FriendList from "./FriendList/FriendList"
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import transactions from "../transactions.json"
+
 
 const App = () => {
   return (
     <div>
-      <Profile 
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        img={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList 
-        friends={friends}/>
-      <TransactionHistory
-        items={transactions}/>
     </div>
       
   )
